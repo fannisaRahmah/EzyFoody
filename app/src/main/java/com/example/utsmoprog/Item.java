d283ea7d6eee664921dacc9b43d35c64ebfd3feb
Fannisa Rahmah <fannisa.azwar2@gmail.com>
2021-11-28 16:57:26 +0700

    first commit

---
+++ app/src/main/java/com/example/utsmoprog/Item.java
@@ -0,0 +1,9 @@
+package com.example.utsmoprog;
+
+public class Item {
+    private Object image;
+    private String name,price;
+
+
+
+}
---
+++ app/src/main/java/com/example/utsmoprog/RecyclerViewAdapter.java
@@ -0,0 +1,14 @@
+package com.example.utsmoprog;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter {
+
+//    public class ViewHolder extends RecyclerView.ViewHolder{
+//        ImageView imageView;
+//        TextView textView;
+//    }
+}
